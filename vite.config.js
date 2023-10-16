2b0ada89a8aabc5a89060086eefd4ab60166a767
Bakhtawar Memon <bakhtawermemon193@gmail.com>
2023-10-17 01:37:49 +0800

    reponamejust

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+
   
-  plugins: [react()],
-  base: "./Bakhtawr/gym_exercises",
+  base: "/gym_exercises/",
+  plugins: [react()]
 
 })
 
